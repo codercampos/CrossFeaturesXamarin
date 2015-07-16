16dfe5ad5eb2269b5a2ec91a88d13f44582a3273
codercampos <carlos.campos@kadevjo.com>
2015-07-15 21:33:39 -0600

    Ups missing file

---
+++ CrossFeaturesXamarin/CrossFeaturesXamarin/CrossFeaturesXamarin/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossFeaturesXamarin.Models
+{
+    public class User
+    {
+        public string ObjectId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public File UserPicture { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        //For sorting items
+        public string NameSort
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Name) || Name.Length == 0) return "?";
+                return Name[0].ToString().ToUpper();
+            }
+        }
+
+        public string ProfilePicture
+        {
+            get
+            {
+                if (UserPicture == null)
+                {
+                    return "item_person_placeholder.jpg";
+                }
+                else
+                {
+                    return UserPicture.Url;
+                }
+            }
+        }
+    }
+}
